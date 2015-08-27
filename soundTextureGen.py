fa97e4fc86767664d67e01f0f37a294259cb65d1
Chen Bingfeng <iichenbf@gmail.com>
2015-08-27 15:58:20 +0800

    v0.1

--- soundTextureGen.py
+++ soundTextureGen.py
@@ -7,14 +7,15 @@ sound/music xxxx.mp3 ---> PCM --->FTT
 ---->Requency Spectrum ----> xxxx.png as Texture for shader
 '''
 
-
+import sys
 from pylab import*
 from scipy.io import wavfile
 from PIL import Image, ImageDraw
 
-TEXTURE_WIDTH = 1024
+TEXTURE_WIDTH = 256 #1024->281min 256->18min max,因为有PNG压缩，所以对图像的大小影响小，但对显存影响还是明显的。
 FPS = 60
-FFT_SCOPE = 1024
+FFT_SCOPE = 1024 #fft 窗大小
+FFT_UNIFORM = 200 #峰值在150左右,200保证安全
 
 
 '''
@@ -26,9 +27,8 @@ FFT_SAMPLE_R = 0.01
 FFT_SAMPLE_G = 0.07
 FFT_SAMPLE_B = 0.15
 FFT_SAMPLE_A = 0.30
-
-
 def genfft(s1):
+    global fft_max
     n = len(s1) 
     p = fft(s1) # take the fourier transform 
     nUniquePts = ceil((n+1)/2.0)
@@ -46,7 +46,7 @@ def genfft(s1):
     else:
         p[1:len(p) -1] = p[1:len(p) - 1] * 2 # we've got even number of points fft
 
-    ret = [10*log10(x) for x in p]
+    ret = [abs(10*log10(x)) for x in p]
     return ret
 '''
 # for v in p:
@@ -72,10 +72,11 @@ def gen(fn):
     #s1是左信道的全部数据 
     print "s1 size =",len(s1)
     
-    img = Image.new("RGBA", (TEXTURE_WIDTH,TEXTURE_WIDTH), color=(0,0,0,0))
+    img = Image.new("RGBA", (TEXTURE_WIDTH,TEXTURE_WIDTH), color=(0,0,0,256))
     draw = ImageDraw.Draw(img)
     
     FFT_STEP = sampFreq*1.0/FPS
+    gfmax = 0.0
     for i in xrange(int(sound_length*FPS)):
         start_index = FFT_STEP*i
         end_index = start_index + FFT_SCOPE
@@ -83,17 +84,29 @@ def gen(fn):
             end_index = len(s1)-1
         fft_data = s1[start_index:end_index]
         fft_ret = genfft(fft_data)
-        print len(fft_ret)
+#         print len(fft_ret)
         fft_ret_len = len(fft_ret)
-        rgba = (fft_ret[int(FFT_SAMPLE_R*fft_ret_len)],
+        rgba = [fft_ret[int(FFT_SAMPLE_R*fft_ret_len)],
                 fft_ret[int(FFT_SAMPLE_G*fft_ret_len)],
                 fft_ret[int(FFT_SAMPLE_B*fft_ret_len)],
-                fft_ret[int(FFT_SAMPLE_A*fft_ret_len)])
+                fft_ret[int(FFT_SAMPLE_A*fft_ret_len)]]
+        for x in rgba:
+            if x > gfmax:
+                gfmax = x
+        rgba = [1.0-x/FFT_UNIFORM for x in rgba]
         ###make uniform
-        draw.point((i%TEXTURE_WIDTH, i/TEXTURE_WIDTH), fill=(int(256*color[0]),
-                                      int(256*color[1]),
-                                      int(256*color[2]),
-                                      int(256*color[3])))
+        draw.point((i%TEXTURE_WIDTH, i/TEXTURE_WIDTH), fill=(int(256*rgba[0]),
+                                      int(256*rgba[1]),
+                                      int(256*rgba[2]),
+                                      int(256*rgba[3])))
+    img.save(fn+".PNG", 'PNG')
+    print "max=",gfmax
 
 if __name__ == "__main__":
-    gen("a.wav")
+    if len(sys.argv) == 2:
+        fn = sys.argv[1]
+        gen(fn)
+    else:
+        print "mp3 file path as parameter..."
+    
+    print 'DONE'
